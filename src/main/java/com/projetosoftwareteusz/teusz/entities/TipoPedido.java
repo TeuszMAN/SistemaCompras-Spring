c0816a5cb8f0186a9403aee02f81a02dececca7f
Mateus Alves <69817312+TeuszMAN@users.noreply.github.com>
2025-11-16 21:25:22 +0000

    Create PedidoFactory and TipoPedido

---
+++ src/main/java/com/projetosoftwareteusz/teusz/entities/TipoPedido.java
@@ -0,0 +1,7 @@
+package com.projetosoftwareteusz.teusz.entities;
+
+public enum TipoPedido {
+
+    COMUM, EXPRESSO
+
+}
--- src/main/java/com/projetosoftwareteusz/teusz/factory/PedidoFactory.java
+++ src/main/java/com/projetosoftwareteusz/teusz/factory/PedidoFactory.java
@@ -1,5 +1,20 @@
 package com.projetosoftwareteusz.teusz.factory;
 
+import com.projetosoftwareteusz.teusz.entities.TipoPedido;
+import com.projetosoftwareteusz.teusz.pedido.IPedido;
+import com.projetosoftwareteusz.teusz.pedido.PedidoComum;
+import com.projetosoftwareteusz.teusz.pedido.PedidoExpresso;
+
 public class PedidoFactory {
+
+
+    public IPedido criarPedido(TipoPedido tipo) {
+        if (tipo == TipoPedido.COMUM) {
+            return new PedidoComum();
+        } else if (tipo == TipoPedido.EXPRESSO) {
+            return new PedidoExpresso();
+        }
+        return null;
+    }
 
 }
