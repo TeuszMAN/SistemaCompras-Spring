185e5af33e7b956bdf1900103fd50ea749dfd22d
Mateus Alves <69817312+TeuszMAN@users.noreply.github.com>
2025-11-16 21:17:18 +0000

    Update pom and create Inteface pedido

--- src/main/java/com/projetosoftwareteusz/teusz/entities/Produto.java
+++ src/main/java/com/projetosoftwareteusz/teusz/entities/Produto.java
@@ -1,5 +1,39 @@
 package com.projetosoftwareteusz.teusz.entities;
 
-public class Pessoa {
+public class Produto {
+
+    private int id;
+    private String nome;
+    private double preco;
+
+    public Produto(int id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+    public String toString() {
+        return "Produto{id=" + id + ", nome='" + nome + "', preco=" + preco + "}";
+    }
 
 }
---
+++ src/main/java/com/projetosoftwareteusz/teusz/pedido/IPedido.java
@@ -0,0 +1,7 @@
+package com.projetosoftwareteusz.teusz.pedido;
+
+public interface IPedido {
+
+    public void processarPedido();
+
+}
